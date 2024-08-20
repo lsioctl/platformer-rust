4d05b2d60eb2b2e3cc0d29842b6809d5b668e6e3
lsioctl <lsioctl@gmail.com>
2024-08-20 19:17:26 +0200

    added background

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,19 @@ use raylib::prelude::*;
 fn main() {
     let mut last_frame_time = Instant::now();
 
-    let (mut rl, thread) = raylib::init().size(640, 480).title("Hello, World").build();
+    let (mut rl, thread) = raylib::init().size(800, 480).title("Hello, World").build();
 
     let mut player = player::Player::new(&mut rl, &thread);
 
     rl.set_target_fps(60);
+
+    let background = rl
+        .load_texture(&thread, "./ressources/Backgrounds/Layer0_0.png")
+        .unwrap();
+
+    let background2 = rl
+        .load_texture(&thread, "./ressources/Backgrounds/Layer1_0.png")
+        .unwrap();
 
     // Detect window close button or ESC key
     while !rl.window_should_close() {
@@ -26,6 +34,8 @@ fn main() {
         let mut d = rl.begin_drawing(&thread);
 
         d.clear_background(Color::WHITE);
+        d.draw_texture(&background, 0, 0, Color::WHITE);
+        d.draw_texture(&background2, 0, 0, Color::WHITE);
         d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
 
         player.draw(&mut d);
