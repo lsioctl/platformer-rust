81cdebb92a1ac5ae2b49a118e524d45cca975bf1
lsioctl <lsioctl@gmail.com>
2024-08-21 11:16:29 +0200

    clumsy but working parallax

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,9 @@ fn main() {
         .load_texture(&thread, "./ressources/Backgrounds/Layer1_0.png")
         .unwrap();
 
+    let mut scrolling_back = 0.as_f32();
+    let mut scrolling_fore = 0.as_f32();
+
     // Detect window close button or ESC key
     while !rl.window_should_close() {
         // delta_time: TODO some recommend using ticks instead
@@ -33,10 +36,59 @@ fn main() {
 
         let mut d = rl.begin_drawing(&thread);
 
+        scrolling_back = scrolling_back - 0.1;
+        scrolling_fore = scrolling_fore - 0.5;
+
+        if scrolling_back <= -background.width.as_f32() {
+            scrolling_back = 0.as_f32();
+        };
+        if scrolling_fore <= -background2.width.as_f32() {
+            scrolling_fore = 0.as_f32();
+        }
+
         d.clear_background(Color::WHITE);
-        d.draw_texture(&background, 0, 0, Color::WHITE);
-        d.draw_texture(&background2, 0, 0, Color::WHITE);
-        d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+
+        d.draw_texture_ex(
+            &background,
+            Vector2 {
+                x: scrolling_back,
+                y: 0.,
+            },
+            0.,
+            1.,
+            Color::WHITE,
+        );
+        d.draw_texture_ex(
+            &background,
+            Vector2 {
+                x: background.width.as_f32() + scrolling_back,
+                y: 0.,
+            },
+            0.,
+            1.,
+            Color::WHITE,
+        );
+
+        d.draw_texture_ex(
+            &background2,
+            Vector2 {
+                x: scrolling_fore,
+                y: 0.,
+            },
+            0.,
+            1.0,
+            Color::WHITE,
+        );
+        d.draw_texture_ex(
+            &background2,
+            Vector2 {
+                x: background2.width.as_f32() + scrolling_fore,
+                y: 0.,
+            },
+            0.,
+            1.0,
+            Color::WHITE,
+        );
 
         player.draw(&mut d);
     }
