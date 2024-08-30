808effe8a44ec9719a01542f26164cdbd3ec5d3b
lsioctl <lsioctl@gmail.com>
2024-08-30 19:00:48 +0200

    proper parallax

--- src/background.rs
+++ src/background.rs
@@ -2,10 +2,11 @@ use raylib::prelude::*;
 
 pub struct Background {
     texture_back_list: Vec<Texture2D>,
-    texture_front: Texture2D,
+    texture_mid_list: Vec<Texture2D>,
     scrolling_back: f32,
-    scrolling_front: f32,
+    scrolling_mid: f32,
     back_index: usize,
+    mid_index: usize,
 }
 
 impl Background {
@@ -20,29 +21,39 @@ impl Background {
             .load_texture(&thread, "./ressources/Backgrounds/Layer0_2.png")
             .unwrap();
 
+        let texture_mid0 = rl
+            .load_texture(&thread, "./ressources/Backgrounds/Layer1_0.png")
+            .unwrap();
+        let texture_mid1 = rl
+            .load_texture(&thread, "./ressources/Backgrounds/Layer1_1.png")
+            .unwrap();
+        let texture_mid2 = rl
+            .load_texture(&thread, "./ressources/Backgrounds/Layer1_2.png")
+            .unwrap();
+
         Self {
             texture_back_list: vec![texture_back0, texture_back1, texture_back2],
-            texture_front: rl
-                .load_texture(&thread, "./ressources/Backgrounds/Layer1_0.png")
-                .unwrap(),
+            texture_mid_list: vec![texture_mid0, texture_mid1, texture_mid2],
             scrolling_back: 0.0,
-            scrolling_front: 0.0,
+            scrolling_mid: 0.0,
             back_index: 0,
+            mid_index: 0,
         }
     }
 
     // Draw backgound with parallax
     pub fn draw(&mut self, d: &mut RaylibDrawHandle) {
         self.scrolling_back = self.scrolling_back - 0.5;
-        // self.scrolling_front = self.scrolling_front - 0.5;
+        self.scrolling_mid = self.scrolling_mid - 0.8;
         // println!("{}", self.scrolling_back);
 
         if self.scrolling_back <= -self.texture_back_list[self.back_index].width.as_f32() {
             self.back_index = (self.back_index + 1) % 3;
             self.scrolling_back = 0.as_f32();
         };
-        if self.scrolling_front <= -self.texture_front.width.as_f32() {
-            self.scrolling_front = 0.as_f32();
+        if self.scrolling_mid <= -self.texture_mid_list[self.mid_index].width.as_f32() {
+            self.mid_index = (self.mid_index + 1) % 3;
+            self.scrolling_mid = 0.as_f32();
         }
 
         d.draw_texture_ex(
@@ -67,9 +78,9 @@ impl Background {
         );
 
         d.draw_texture_ex(
-            &self.texture_front,
+            &self.texture_mid_list[self.mid_index],
             Vector2 {
-                x: self.scrolling_front,
+                x: self.scrolling_mid,
                 y: 0.,
             },
             0.,
@@ -77,9 +88,9 @@ impl Background {
             Color::WHITE,
         );
         d.draw_texture_ex(
-            &self.texture_front,
+            &self.texture_mid_list[(self.mid_index + 1) % 3],
             Vector2 {
-                x: self.texture_front.width.as_f32() + self.scrolling_front,
+                x: self.texture_mid_list[self.mid_index].width.as_f32() + self.scrolling_mid,
                 y: 0.,
             },
             0.,
